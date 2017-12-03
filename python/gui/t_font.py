f80b698771b375a774fe97a7fec67f48ac7cd9ae
taowuwen <taowuwen@gmail.com>
2017-12-04 00:08:13 +0800

    support for font

---
+++ python/gui/t_font.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tkinter
+from tkinter import font
+
+def main():
+    top = tkinter.Tk()
+
+    print("hello, fonts test")
+
+    print(font.families())
+    print(font.names())
+    pass
+
+
+if __name__ == '__main__':
+    main()

2cec65246859348cf2e483bcb519dcdde4cd0971
taowuwen <taowuwen@gmail.com>
2018-01-12 00:23:49 +0800

    update

--- http_server/uhttpd-2012-10-30/startuhttp.sh
+++ http_server/uhttpd-2012-10-30/startuhttp.sh
@@ -2,6 +2,6 @@
 
 ROOT=${HOME}/codec/html
 
-./uhttpd  -s 0.0.0.0:80 -n 20 -T 10 -h /home/tww -x /cgi-bin
+./uhttpd  -s 0.0.0.0:80 -n 20 -T 10 -h /tmp -x /cgi-bin
 
 #-i .lua=/usr/bin/lua -i .sh=/usr/bash -i .py=/usr/bin/python -f &

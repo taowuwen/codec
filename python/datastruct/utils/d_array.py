21429bde9062774b3839894ab139734b681d64a2
taowuwen <taowuwen@gmail.com>
2018-02-26 20:01:39 +0800

    init array

--- python/datastruct/utils/d_array.py
+++ python/datastruct/utils/d_array.py
@@ -15,7 +15,6 @@ class CArray:
         self._data = [None for i in range(length)]
         self._base = base
 
-
     def __getitem__(self, index):
         return self._data[index]
 
@@ -25,6 +24,31 @@ class CArray:
     def __len__(self):
         return len(self._data)
 
-    property
+    def set_length(self, l, base=0):
+
+        if l != len(self._data):
+
+            _data = [None for i in range(l)]
+
+            _l = min(len(self._data), l)
+            for i in range(_l):
+                _data[i] = self._data[i]
+
+            self._data = _data
+
+        self._base = base
+
+    length = property(fget=__len__, fset=set_length)
+
+    def __str__(self):
+        return str(self._data)
 
 
+
+if __name__ == '__main__':
+
+    a = CArray(10)
+    print(a.length)
+    a[0] = 'aaa'
+    a[2] = 'bbbccc'
+    print(a)

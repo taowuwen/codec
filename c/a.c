cf033b16ee80c7d953059af4fff06df8243e8b56
tww <taowuwen@gmail.com>
2018-04-18 11:19:15 +0800

    update

---
+++ c/a.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc, char **argv) 
+{
+	int i, n;
+	int number = 0;
+	double s, sum = 0;
+
+	printf("Please input a number for n: \n");
+	scanf("%d", &n);
+	printf("The number is n=%d\n", n);
+
+	for (i = 3; i < n; i++) {
+		if (i % 21 == 0) {
+			sum += (double)i;
+			number++;
+		}
+
+		/*
+		if (i%3 == 0 && i % 7 == 0) {
+		}
+		*/
+	}
+
+	printf("sum = %lf, sqrt(%lf) = %f\n", sum, sqrt(sum)); 
+	printf("Number = %d\n", number);
+	return 0;
+}

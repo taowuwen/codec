2a615a36ab9f2803d03e3c34da5e0d90e10afaef
tww <taowuwen@gmail.com>
2018-05-07 10:15:05 +0800

    update

--- http_server/uhttpd-2012-10-30/startuhttp.sh
+++ http_server/uhttpd-2012-10-30/startuhttp.sh
@@ -2,6 +2,10 @@
 
 ROOT=${HOME}/codec/html
 
-./uhttpd  -s 0.0.0.0:8880 -n 20 -T 10 -h $ROOT -x /cgi-bin -i .py=/usr/bin/python3 -I index.py -I index.html -I index.htm
+bind_address="-s 0.0.0.0:18880"
+
+[ `id -u` -eq 0 ] && bind_address="$bind_address -s 0.0.0.0:80"
+
+./uhttpd  $bind_address -n 20 -T 10 -h $ROOT -x /cgi-bin -i .py=/usr/bin/python3 -I index.py -I index.html -I index.htm
 
 #-i .lua=/usr/bin/lua -i .sh=/usr/bash -i .py=/usr/bin/python -f &

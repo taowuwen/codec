7501512a0684a272819bf9a9cbdeaa67296f2c9f
taowuwen <taowuwen@gmail.com>
2019-02-17 16:33:45 +0800

    update

--- python/test.py
+++ python/test.py
@@ -1,57 +1,53 @@
 #!/usr/bin/env python
 
 
-
 class CTestProperty():
-	def __init__(self):
-		self._x = 100
+    def __init__(self):
+        self._x = 100
 
-	@property
-	def x(self):
-		return self._x
+    @property
+    def x(self):
+        return self._x
 
-	@x.setter
-	def x(self, value):
-		self._x = value
+    @x.setter
+    def x(self, value):
+        self._x = value
+
 
 class CTestProperty_v2(object):
-	def __init__(self):
-		self._x = 100
+    def __init__(self):
+        self._x = 100
 
-	@property
-	def x(self):
-		return self._x
+    @property
+    def x(self):
+        return self._x
 
-	@x.setter
-	def x(self, value):
-		self._x = value
+    @x.setter
+    def x(self, value):
+        self._x = value
 
-	@x.deleter
-	def x(self):
-		del self._x
+    @x.deleter
+    def x(self):
+        del self._x
 
 
 def main():
-	p = CTestProperty()
+    p = CTestProperty()
 
-	print("x is: %d"%(p.x))
+    print("x is: %d" % (p.x))
 
-	p.x = 10
-	print("x is: %d"%(p.x))
+    p.x = 10
+    print("x is: %d" % (p.x))
 
-	p = CTestProperty_v2()
+    p = CTestProperty_v2()
 
-	print("x is: %d"%(p.x))
+    print("x is: %d" % (p.x))
 
-	p.x = 10
-	print("x is: %d"%(p.x))
+    p.x = 10
+    print("x is: %d" % (p.x))
 
-	del p.x
-
+    del p.x
 
 
 if __name__ == '__main__':
-	main()
-
-
-
+    main()

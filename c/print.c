6d1b5023041a5d126597f54847bb119c471a9925
taowuwen <taowuwen@gmail.com>
2019-06-22 19:59:07 +0800

    update print.c

--- c/print.c
+++ c/print.c
@@ -5,12 +5,12 @@
 int main(int argc, char *argv[])
 {
 	int len;
-	char *ptr = "hello, world";
+	char ptr_str[] = "hello, world";
 
-	len = strlen(ptr);
+	printf("strlen(ptr_str): %d\n", strlen(ptr_str));
 
-	printf("===> %.*s\n",len - 1, ptr);
-	printf("===> %s\n", ptr);
+	printf("===> %.*s\n",sizeof(ptr_str) - 2, ptr_str);
+	printf("===> %s\n", ptr_str);
 
 	return 0;
 }
